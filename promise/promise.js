be93d8b9be7a70df9cfac76b0d945f8686e4650a
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-03-13 21:03:06 -0700

    Add more functions

--- promise/promise.js
+++ promise/promise.js
@@ -1,18 +1,20 @@
-console.log("Program Start");
+const axios = require('axios');
 
-const doStuff = function(callback, text) {
+console.log("User Thread Start");
+
+const doStuff = function (callback, text) {
 	callback(text);
 };
 
-const longRunningFunction = function(value) {
-	let promise = new Promise(function(resolve, reject) {
+const longRunningFunction = function (value) {
+	let promise = new Promise(function (resolve, reject) {
 		// Do something that takes time.
 		if (value > 0) {
 			console.log("start timeout: ", value);
 			// setTimeout(doStuff(resolve, "yay that worked:"+value), value);
 			setTimeout(() => resolve("yay that worked:" + value), value);
 		} else {
-			setTimeout(doStuff(reject, "oh no! no value"), 500);
+			setTimeout(doStuff(reject, "oh no!:" + value), 500);
 			// setTimeout(() => reject("error, no value"), 500);
 		}
 	});
@@ -20,23 +22,98 @@ const longRunningFunction = function(value) {
 	return promise;
 };
 
-console.log("calling longRunningFunction()");
+const main1 = function () {
+	console.log("calling longRunningFunction()");
 
-const status = longRunningFunction(220)
-	.then(result => console.log(result))
-	.catch(error => console.log(error));
+	const status = longRunningFunction(220)
+		.then(result => console.log(result))
+		.catch(error => console.log(error))
+		.finally(() => console.log(this.promise1));
 
-console.log(status);
+	console.log(status);
+};
 
-promise1 = longRunningFunction(3330).catch(e => e);
-promise2 = longRunningFunction(0).catch(e => e);
-promise3 = longRunningFunction(2200).catch(e => e);
+const main2 = function () {
+	console.log("calling multiple longRunningFunction's");
+	promise1 = longRunningFunction(1330).catch(e => e);
+	promise2 = longRunningFunction(0).catch(e => e);
+	promise3 = longRunningFunction(200).catch(e => e);
 
-console.log("calling multiple promises");
-Promise.all([promise1, promise2, promise3])
-	.then(result => console.log(result))
-	.catch(error => console.log(error));
+	console.log("calling multiple promises");
+	const status = Promise.all([promise1, promise2, promise3])
+		.then(result => console.log(result))
+		.catch(error => console.log(error))
+		.finally(() => console.log(this.promise1));
 
-// Program flow drops down to here on main thread
+	console.log(status);
+};
 
-console.log("End of user thread");
+const main3 = function () {
+
+	axios.get('https://meowfacts.herokuapp.com/')
+		.then(res => {
+			console.log(res.data.data[0]);
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+
+};
+
+const callAxios = function (url) {
+
+	const promise = axios.get(url)
+		.then(function (res) { return res.data; });		// can't trick js into making async sync :-)
+
+	// return promise;  // Maybe I can trick js into waiting to return :)
+};
+
+const main4 = function () {
+
+	const status = callAxios('https://meowfacts.herokuapp.com/')
+		.then(res => console.log(res));
+
+	console.log(status);
+
+};
+
+
+
+const main5 = function () {
+
+	const promise1 = axios.get('https://meowfacts.herokuapp.com/').catch(e => e);
+	const promise2 = axios.get('https://meowfacts.herokuapp.com/').catch(e => e);
+	const promise3 = axios.get('https://meowfacts.herokuapp.com/').catch(e => e);
+
+	const status = Promise.all([promise1, promise2, promise3])
+		.then(responses => {
+			console.log(responses.length);
+			for (const res of responses) {
+				if (res.data)
+					console.log(res.data.data[0]);
+				else
+					console.log(res.response.status);
+			}
+		}
+		)
+		.catch(error => console.log(error.response.status));
+
+	console.log(status);
+};
+
+
+const main6 = async function () {
+
+	try {
+		const status = axios('https://meowfacts.herokuapp.com/');
+		console.log((await status).data.data[0]);
+	}
+	catch (e) {
+		console.log(e.response);
+	}
+
+};
+
+
+main2();
+console.log("User Thread End");
